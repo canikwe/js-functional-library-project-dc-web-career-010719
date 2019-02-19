95f0de431ff644980e3a1f42cb6fa0ae4bb6ab9d
canikwe <c.anikwe@gmail.com>
2019-02-19 14:43:33 -0500

    Update each and map

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+arr = [1, 2, 3, 4]
+
 fi = (function() {
   return {
     libraryMethod: function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+        for(const el of collection) {
+            callback(el)
+        }
+        return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+        const newArray = []
+        for(const el of collection){
+            newArray.push(callback(el))
+        }
+        return newArray
     },
 
     reduce: function() {
